102e64837475ce432aa3ead4ab5b31ccf30075d7
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-12 09:55:14 +0300

    init

--- task5/ContentView.swift
+++ task5/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var offset = CGSize.zero
+    private let height = UIScreen.main.bounds.height / 4
+    private let width = UIScreen.main.bounds.width
+    
+    var gesture: some Gesture {
+        DragGesture()
+            .onChanged { gesture in
+                offset = gesture.translation
+            }
+            .onEnded { _ in
+                withAnimation(.spring()) {
+                    offset = .zero
+                }
+            }
+    }
+    
+    var rectangle: some View {
+        Rectangle()
+            .foregroundColor(.white)
+            .blendMode(.difference)
+            .overlay(Rectangle().blendMode(.hue))
+            .overlay(Rectangle().foregroundStyle(.white).blendMode(.overlay))
+            .overlay(Rectangle().foregroundStyle(.black).blendMode(.overlay))
+            .clipShape(
+                RoundedRectangle(
+                    cornerRadius: 20,
+                    style: .continuous
+                ))
+            .frame(width: 150, height: 150)
+            .offset(offset)
+            .gesture(gesture)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0) {
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundStyle(.white)
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundStyle(.pink)
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundStyle(.yellow)
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundStyle(.black)
+            }
+            rectangle
         }
-        .padding()
     }
 }
 
